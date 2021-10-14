171094eb6c2598a118387481a0ac3c8c4ed62ec0
Поддубко Сергей <snpod@rambler.ru>
2021-10-14 22:37:53 +1000

    kity_bot

---
+++ kittybot.py
@@ -0,0 +1,95 @@
+# kittybot/kittybot.py
+
+
+# from telegram import Bot
+
+# # Здесь укажите токен, 
+# # который вы получили от @Botfather при создании бот-аккаунта
+# bot = Bot(token='2076140953:AAEZIuiG2YbwP9RnWhLem9RiVX7v77mxjJU')
+# # Укажите id своего аккаунта в Telegram
+# chat_id = 522743432
+# text = 'Вам телеграмма!'
+# # Отправка сообщения
+# bot.send_message(chat_id, text)
+
+# # kittyboyt/kittybot.py
+import logging
+import os
+import requests
+from telegram.ext import Updater, Filters, MessageHandler, CommandHandler
+from telegram import ReplyKeyboardMarkup
+
+from dotenv import load_dotenv
+
+load_dotenv()
+auth_token = os.getenv('TOKEN')
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+#print(auth_token)
+updater = Updater(token=auth_token)
+URL = 'https://api.thecatapi.com/v1/images/search'
+#URL = ''
+
+def get_new_image():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        #print(error)      
+        logging.error(f'Ошибка при запросе к основному API: {error}')
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    response = response.json()
+    random_cat = response[0].get('url')
+    return random_cat
+
+def new_cat(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def say_hi(update, context):
+    chat = update.effective_chat
+    context.bot.send_message(chat_id=chat.id,text='Привет, я KittyBot!')
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/newcat']], resize_keyboard=True)
+    # buttons = ReplyKeyboardMarkup([
+    #             ['Который час?', 'Определи мой ip'],
+    #             ['/random_digit']
+    #        ])
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Посмотри, какого котика я тебе нашёл'.format(name),
+        reply_markup=button
+
+        # text='Спасибо, что вы включили меня, {}!'.format(name),
+        # reply_markup=buttons
+        )
+    context.bot.send_photo(chat.id, get_new_image())
+
+
+def i_want(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    context.bot.send_message(chat_id=chat.id, 
+                             text=f'Я хочу тебя {name} !!!')
+# Регистрируется обработчик CommandHandler;
+# он будет отфильтровывать только сообщения с содержимым '/start'
+# и передавать их в функцию wake_up()
+def main():
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(CommandHandler('newcat', new_cat))
+    updater.dispatcher.add_handler(CommandHandler('sex', i_want))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
+
+    updater.start_polling()
+    # updater.start_polling(poll_interval=20.0) 
+    updater.idle()
+
+if __name__ == '__main__':
+    main()  
